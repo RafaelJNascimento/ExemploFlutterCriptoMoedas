960b77d5a430aa1cf0214d4035d2cdb219448398
Rafael José do Nascimento <eng.rafaeljosedonascimento@gmail.com>
2022-10-11 22:14:20 -0300

    [FEATURE] Como selecionar um item na ListView | Curso de Flutter

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,8 +1,21 @@
+import 'package:cripto_moeda/lib/model/modeda.dart';
 import 'package:cripto_moeda/lib/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MoedasPage extends StatelessWidget {
-  const MoedasPage({super.key});
+class MoedasPage extends StatefulWidget {
+  MoedasPage({super.key});
+
+  @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  final tabela = MoedaRepository.tabela;
+
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  List<Moeda> selecionadas = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +28,38 @@ class MoedasPage extends StatelessWidget {
       body: ListView.separated(
         itemBuilder: (BuildContext context, int moeda) {
           return ListTile(
-            leading: Image.asset(tabela[moeda].icone),
-            title: Text(tabela[moeda].nome),
-            trailing: Text(tabela[moeda].preco.toString()),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            leading: (selecionadas.contains(tabela[moeda]))
+                ? const CircleAvatar(
+                    child: Icon(
+                      Icons.check,
+                    ),
+                  )
+                : SizedBox(
+                    width: 40,
+                    child: Image.asset(tabela[moeda].icone),
+                  ),
+            title: Text(
+              tabela[moeda].nome,
+              style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            trailing: Text(real.format(tabela[moeda].preco)),
+            selected: selecionadas.contains(tabela[moeda]),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              setState(() {
+                selecionadas.contains(tabela[moeda])
+                    ? selecionadas.remove(tabela[moeda])
+                    : selecionadas.add(tabela[moeda]);
+              });
+            },
           );
         },
         padding: const EdgeInsets.all(16),
